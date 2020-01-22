eadc5fcdd736823c100aeb65141bb278f9953e48
codeasaglacier <remingtonshock@gmail.com>
2020-01-21 16:39:44 -0900

    set up app

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './form'
 
-function App() {
+export default function App() {
+
+const [ team, setTeam ] = useState([])
 
   return (
     <div className="App">
-      <Form />
+      <Form  setTeam={setTeam}/>
+      <h1>Team Members</h1>
+      {etam.map(member => {
+        return (
+          <div className='member'>
+            <h1>{member.name}</h1>
+          </div>
+        )
+      })}
     </div>
       
   );
 }
 
-export default App;
+
